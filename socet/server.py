d44dd24e6f9e062743ca231eb519632fe82e10b6
Рома Жуков <romazukov@MacBook-Air-Roma.local>
2023-05-07 20:48:36 +0300

    add log system to server.py

--- socet/server.py
+++ socet/server.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify, request
+import logging as log
+
+log.basicConfig(level = log.INFO, filename = 'log/log.log', filemode = 'a', format = "%(asctime)s %(levelname)s %(message)s")
 
 app = Flask(__name__)
+
+log.info("Server started")
 
 @app.route('/chat', methods = ['POST'])
 
@@ -15,6 +20,8 @@ def chat():
     }
 
     if message in dt.keys():
+        log.warning("Message isn't in dictionary")
+        log.error("I don't know xD")
         return jsonify(dt[message])
     else:
         return jsonify('Hi')
